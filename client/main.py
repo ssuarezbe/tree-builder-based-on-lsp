0cf809f4b02e8d49a6e7f17ffba95a8bc8efee0e
Sebastian Suarez <ssuarezbe@gmail.com>
2024-10-17 16:11:36 -0500

    - basic client communication working with https://github.com/ssuarezbe/VBA-LanguageServer/tree/83cd6d407d8ab76671751c7cff0304e4f0bdb975 	modified:   client/main.py

--- client/main.py
+++ client/main.py
@@ -27,6 +27,13 @@ from uuid_v7.base import uuid7
 
 src_code_folder = os.path.join('tmp','lsp','vba')
 
+
+"""
+LSP server lifetime :
+
+* https://microsoft.github.io/language-server-protocol/specifications/specification-3-15/#initialize
+"""
+
 def get_initialize_lsp_msg():
     """
     Example of simple initializeParams:
@@ -75,8 +82,66 @@ def get_initialize_lsp_msg():
     }
     return json.dumps(json_msg)
 
-def get_document_symbols_msg(filename):
+def get_open_document_on_lsp_server_msg(filename:str, file_content:str):
     """
+    Client send a request to the server to set a document as ._activeDocument
+
+    https://microsoft.github.io/language-server-protocol/specifications/specification-3-15/#textDocument_didOpen 
+
+    textDocument/didClose
+
+
+    the method DOES exists:
+
+    https://github.com/microsoft/vscode-languageserver-node/blob/a561f1342ba94ad7f550cb15446f65432f5e1367/protocol/src/common/protocol.ts#L1961
+    """
+    call_id:str = str(uuid7())
+    close_json_msg = {
+        "jsonrpc": "2.0",
+        "id":call_id,
+        "method": "textDocument/didClose",
+        "params": {
+            "textDocument": {
+                "uri":  f'file:///{filename}',
+                "languageId":"vb",
+                "text": file_content
+                }
+        }
+    }
+    # call example https://github.com/Dart-Code/Dart-Code/issues/3625
+    open_json_msg = {
+        "jsonrpc": "2.0",
+        "id":call_id,
+        "method": "textDocument/didOpen",
+        "params": {
+            "textDocument": {
+                "uri":  f'file:///{filename}',
+                "languageId":"vb",
+                "text": file_content
+                }
+        }
+    }
+
+    change_json_msg = {
+        "jsonrpc": "2.0",
+        "id":call_id,
+        "method": "textDocument/didChange",
+        "params": {
+            "textDocument": {
+                "uri":  f'file:///{filename}',
+                "languageId":"vb",
+                "text": file_content
+                }
+        }
+    }
+    return (json.dumps(close_json_msg), json.dumps(open_json_msg), json.dumps(change_json_msg))
+
+def get_document_symbols_msg(filename:str):
+    """
+    Documentation:
+
+    * https://microsoft.github.io/language-server-protocol/specifications/specification-3-15/#textDocument_documentSymbol
+
     Example of basic DocumentSymbolParams :
 
     * https://github.com/microsoft/vscode-languageserver-node/blob/a561f1342ba94ad7f550cb15446f65432f5e1367/protocol/src/node/test/connection.test.ts#L115
@@ -91,6 +156,11 @@ def get_document_symbols_msg(filename):
     * https://github.com/search?q=repo%3Amicrosoft%2Fvscode-languageserver-node%20provideDocumentSymbols&type=code
     """
     call_id:str = str(uuid7())
+    """
+    How enalble a document into a LSP server:
+    * https://stackoverflow.com/questions/76756132/python-client-library-for-language-server-protocol-lsp
+    """
+    
     json_msg = {
         "jsonrpc": "2.0",
         "id":call_id,
@@ -112,9 +182,19 @@ async def client(uri):
         await websocket.send(msg)
         resp = await websocket.recv()
         print(f'<- RESPONSE: {resp}')
+        print("\n")
 
     await send_recv(get_initialize_lsp_msg())
-    await send_recv(get_document_symbols_msg('sample01.cls'))
+    #await send_recv(get_document_symbols_msg('sample01.cls'))
+    src_code_file = '/tmp/lsp/vba/sample01.cls'
+    with open(src_code_file, 'r') as f:
+        code_str = f.read()
+    close_msg, open_msg, change_msg = get_open_document_on_lsp_server_msg(src_code_file, code_str)
+    #await send_recv(change_msg)
+    #await send_recv(close_msg)
+    await send_recv(open_msg)
+    await send_recv(get_document_symbols_msg(src_code_file))
+    #await send_recv(close_msg)
     await websocket.close()
 
 asyncio.run(client('ws://localhost:3000/vbaLspSampleServer'))  
